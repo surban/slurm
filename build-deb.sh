03e000d01b303d3d4b733ea0355e91970c55e60c
Sebastian Urban <surban@tum.de>
2014-03-18 15:18:28 +0100

    build script

--- build-deb.sh
+++ build-deb.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-git-buildpackage --git-debian-branch=slurm-2.6-debian --git-upstream-branch=slurm-2.6 --git-upstream-tree=BRANCH       
+git-buildpackage --git-debian-branch=slurm-2.6-debian --git-upstream-branch=slurm-2.6 --git-upstream-tree=BRANCH "--git-builder=debuild -i -I -us -uc"
+  
